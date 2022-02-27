28bee99f9e9f0ccb7f9d671b0396c63a0ceccbba
WIKL <williklaus@gmx.net>
2022-02-27 11:52:33 +0100

    commitz

--- variables.tf
+++ variables.tf
@@ -3,26 +3,26 @@ variable "resource_group_location" {
   description   = "Location of the resource group."
 }
 
-variable  "azure_subscription_id" {
+variable "azure_subscription_id" {
   default = "2192b235-f880-4532-ac54-58e623ce2391"
   description   = "Azure Subcription Id of the principle service"
   type = string
 }
 
-variable  "azure_tenant_id" {
+variable "azure_tenant_id" {
   default = "10f936a0-4fa0-4743-adfb-df02ae115890"
   description   = "Azure tenant Id of the principle service"
   type = string
 }
 
-variable  "service_principal_appid" {
+variable "service_principal_appid" {
   default = "c73ec01c-a93c-479e-ba9e-2a38430cb675"
-  description   = "Client id of the principle service."
+  description = "Client id of the principle service."
   type = string
 }
 
-variable  "service_principal_password" {
+variable "service_principal_password" {
   default = "yCfEYB8mXOuTcB~5_wfJ5hnAQl2xK31mM1"
-  description   = "Client secret of the principle service."
+  description = "Client secret of the principle service."
   type = string
 }
